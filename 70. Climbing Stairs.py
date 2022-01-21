8272d9e2d7a7c6797d9ff800bd5b14bb492495e4
Prashant Rajendra Londhe <90589964+Prashantlondhe123@users.noreply.github.com>
2022-01-21 19:24:11 +0530

    Create 70. Climbing Stairs.py

---
+++ 70. Climbing Stairs.py
@@ -0,0 +1,43 @@
+Problem: '''70. Climbing Stairs
+You are climbing a staircase. It takes n steps to reach the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+ 
+
+Example 1:
+
+Input: n = 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+Example 2:
+
+Input: n = 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+'''
+  
+  #Code:
+  
+  class Solution:
+    def climbStairs(self, n: int) -> int:
+        dp={} #n=>no of ways 
+        def dt(stair):
+            if stair== n:
+                return 1
+            if stair > n:
+                return 0
+            if stair in dp:
+                return dp[stair]
+            
+            dp[stair]=dt(stair+1)+ dt(stair+2)
+            
+            return dp[stair]
+        
+        return dt(0)
+  
